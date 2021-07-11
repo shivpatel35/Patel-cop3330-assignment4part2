cd80de86b5fdec3b384f8ec4818198c552bbbbc3
shivpatel35 <shiv30164@gmail.com>
2021-07-11 17:07:01 -0500

    function addition additem Controller

--- src/main/java/ucf/assignmets/ToDoList.java
+++ src/main/java/ucf/assignmets/ToDoList.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 4 Solution
+ *  Copyright 2021 Shiv Patel
+ */
+
 package ucf.assignmets;
 
 import javafx.application.Application;
@@ -5,28 +10,35 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+//start class for fxmlFile
 public class ToDoList extends Application {
 
     public static void main(String[] args) {
-
         launch(args);
     }
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws Exception{
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("App.fxml"));
+            String url = "ucf/assignmets/ToDoList.fxml";
+            Parent root = FXMLLoader.load(getClass().getClassLoader().getResource(url));
+
+
 
             Scene scene = new Scene(root);
 
+
+            primaryStage.setTitle("Todo List");
             primaryStage.setScene(scene);
-            primaryStage.setTitle("To Do Lists");
             primaryStage.show();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+
+
     }
 }
+
